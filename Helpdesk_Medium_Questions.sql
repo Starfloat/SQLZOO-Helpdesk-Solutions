bb29594687d657af8d88a1aed19f04e124ef8380
beatAffection <53842484+beatAffection@users.noreply.github.com>
2019-08-06 23:14:25 -0400

    Create Helpdesk_Medium_Questions.sql

---
+++ Helpdesk_Medium_Questions.sql
@@ -0,0 +1,58 @@
+--6.
+/*List the Company name and the number of calls for those companies with more than 18 calls.
++------------------+----+
+| Company_name     | cc |
++------------------+----+
+| Gimmick Inc.     | 22 |
+| Hamming Services | 19 |
+| High and Co.     | 20 |
++------------------+----+
+*/
+
+--7.
+/*Find the callers who have never made a call. Show first name and last name
++------------+-----------+
+| first_name | last_name |
++------------+-----------+
+| David      | Jackson   |
+| Ethan      | Phillips  |
++------------+-----------+
+*/
+
+--8.
+/*For each customer show: Company name, contact name, number of calls where the number of calls is fewer than 5
++--------------------+------------+-----------+----+
+| Company_name       | first_name | last_name | nc |
++--------------------+------------+-----------+----+
+| Pitiable Shipping  | Ethan      | McConnell |  4 |
+| Rajab Group        | Emily      | Cooper    |  4 |
+| Somebody Logistics | Ethan      | Phillips  |  2 |
++--------------------+------------+-----------+----+
+*/
+
+--9.
+/*For each shift show the number of staff assigned. Beware that some roles may be NULL and that the same person might have been assigned to multiple roles (The roles are 'Manager', 'Operator', 'Engineer1', 'Engineer2').
++------------+------------+----+
+| Shift_date | Shift_type | cw |
++------------+------------+----+
+| 2017-08-12 | Early      |  4 |
+| 2017-08-12 | Late       |  4 |
+| 2017-08-13 | Early      |  3 |
+| 2017-08-13 | Late       |  2 |
+| 2017-08-14 | Early      |  4 |
+| 2017-08-14 | Late       |  4 |
+| 2017-08-15 | Early      |  4 |
+| 2017-08-15 | Late       |  4 |
+| 2017-08-16 | Early      |  4 |
+| 2017-08-16 | Late       |  4 |
++------------+------------+----+
+*/
+
+--10.
+/*Caller 'Harry' claims that the operator who took his most recent call was abusive and insulting. Find out who took the call (full name) and when.
++------------+-----------+---------------------+
+| first_name | last_name | call_date           |
++------------+-----------+---------------------+
+| Emily      | Best      | 2017-08-16 10:25:00 |
++------------+-----------+---------------------+
+*/
